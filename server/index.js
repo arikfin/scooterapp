f290eb40f6c5c5ae60fa9d99cb2dcfd99f31bb42
arikfin <arikfinkel94@gmail.com>
2022-10-28 09:14:55 +0300

    28102022

---
+++ server/index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+
+dotenv.config();
+const app = express();
+//const port = process.env.SERVERPORT; //not relevant 
+
+app.use(cors());
+app.use(express.json());
+
+
+app.listen(process.env.SERVERPORT, () => {
+    console.log(`Server is running under port ${process.env.SERVERPORT}`);
+})
+
+
+
+
+
